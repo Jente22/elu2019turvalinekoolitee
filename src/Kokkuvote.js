aa1634276c35d61ff8ad473c673adb4c1999c7b7
Jente22 <59165506+Jente22@users.noreply.github.com>
2019-12-24 10:52:33 +0200

    Update Kokkuvote.js

--- src/Kokkuvote.js
+++ src/Kokkuvote.js
@@ -4,7 +4,7 @@ export default class Kokkuvote extends React.Component {
   render() {
     return <div className="mt-3">
       <p>
-        Liiklusohutus on valdkond, mis vajab pidevat järelvalvet, objektiivset ülevaadet kõige hiljutisemast seisukorrast ning muidugi ka korduvat ohukohtade kontrolli. Probleemid tulenevad mitmetest erinevatest põhjustest. Murekohtadeks on nii autode eelistamine muule transpordile, mis tekitab tipptundide ajal suuremõõtmelisi ummikuid, asutuste ligiduses asuvad ohtlikud ristmikud ja reguleerimata ületuskohad, lisaks ka jalakäijate võimalik tähelepanematus ümbritseva suhtes. Nimekiri probleemidest, mida igapäevaliikluses ette tuleb, on pikk ja kõige põhjalik käsitlemine on pikaajaline protsess, mida ühe projekti raames oleks keeruline läbi viia. Seetõttu keskendus ELU projekt “turvaline koolitee” ühe kooli õpilaste ja nende vanemate liiklusohutuse alaste arvamuste uurimisele ning analüüsile.
+        Liiklusohutus on valdkond, mis vajab pidevat järelvalvet, objektiivset ülevaadet kõige hiljutisemast seisukorrast ning muidugi ka korduvat ohukohtade kontrolli. Probleemid tulenevad mitmetest erinevatest põhjustest. Murekohtadeks on nii autode eelistamine muule transpordile, mis tekitab tipptundide ajal suuremõõtmelisi ummikuid, asutuste ligiduses asuvad ohtlikud ristmikud ja reguleerimata ületuskohad, lisaks ka jalakäijate võimalik tähelepanematus ümbritseva suhtes. Nimekiri probleemidest, mida igapäevaliikluses ette tuleb, on pikk ja kõige põhjalik käsitlemine on pikaajaline protsess, mida ühe projekti raames oleks keeruline läbi viia. Seetõttu keskendus ELU projekt “Turvaline Koolitee” ühe kooli õpilaste ja nende vanemate liiklusohutuse alaste arvamuste uurimisele ning analüüsile.
       </p>
       
       <p>
