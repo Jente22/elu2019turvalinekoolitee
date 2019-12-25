54876ca5f8efa058a74b6249a4587d0e7474c545
Jente22 <59165506+Jente22@users.noreply.github.com>
2019-12-25 18:27:00 +0200

    katsetasin väikselt

--- src/Analuus.js
+++ src/Analuus.js
@@ -8,3 +8,9 @@ export default class Analuus extends React.Component {
 
   }
 }
+<p>Küsitluste tulemused on eelnevatel lehtedel muudetud interaktiivseteks.  Küsimusele klikkides ilmuvad tulemused tulpdiagrammide näol, millele liikudes näeb numbrilist väärtust.</p> 
+
+<h4>Järveotsa Gümnaasiumi õpilaste küsitluse tulemused ja tulemuste analüüs</h4>
+
+<p> Järveotsa Gümnaasiumis vastas küsimustikule 98 õpilast. Koolis on pisut üle 700 õpilase, seega võib osalusaktiivsust 15% ajalist piirangut arvestades, üsna heaks. Vastajatest 43 olid poisid, 55 tüdrukud. Vanuserühmade lõikes jaotusid vastajad järgmiselt: 1.-3. klassi õpilasi oli vastajate seas 11; 4.-6. klassi õpilasi 25; 7.-9. klassi õpilasi 42 ja 10.-12. klassi õpilasi 20. .</p>  
+
