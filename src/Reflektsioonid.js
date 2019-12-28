6ddd4a5fd10742fc9f11a4c63066d4d51255d8ec
Jente22 <59165506+Jente22@users.noreply.github.com>
2019-12-28 13:54:36 +0200

    Update Reflektsioonid.js

--- src/Reflektsioonid.js
+++ src/Reflektsioonid.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default class Reflektsioonid extends React.Component {
   render() {
     return <div className="mt-3">
-      <p>
-        Siia pane oma tekst
-      </p>
+      <p>Ines Safjanova:</p> 
+    <p>Projekt oli teema ning olemuse poolest üsnagi huvitav ning kuna ma ei ole Tallinnast pärit, sain projekti jooksul sügavamalt aru, milline on Tallinna liikluse hetkeolukord. Seda eriti just koolimajade piirkonnas. Projekti kulgemine läks aga üle kivide ja kändude, rühmaliikmetele tuli korduvalt meelde tuletada, millised ülesanded on veel täitmata ning millised teised üldised küsimused vajaksid lahendamist. Lisaks pidime mitmel korral kiirustama, et kirjalikud tööd saaksid kindlasti tähtajaks esitatud, mis mõningal määral mõjutas meie kõigi nähtud vaeva negatiivselt. Ükski etapp aga ei jäänud sooritamata, samuti saime kokku kogutud kõik vajalikud andmed jms.; mistõttu võib projekti minu silmis siiski õnnestunuks lugeda. Isiklikust vaatenurgast leian, et õppisin projektist peamiselt oskust rühma juhtida ja suutlikust olla kogu rühmatöö organiseerimise eest vastutav isik. Nimetatud õpiväljundid on väga kasulikud tulevastes suurema mastaabiga rühmatöödes, kus võib taaskord juhtuda, et pean asjaolude sunnil juhtimise enda peale võtma. Projektile aitas minu silmis palju kaasa ka asjaolu, et eesmärke täideti kolme grupi koostööna. Tänu sellele olid projekti tulemused asjakohasemad ning ühiselt sai tehtud ära palju rohkem, kui seda oleks suutnud teha iga rühm omaette.</p>
     </div>
 
   }
