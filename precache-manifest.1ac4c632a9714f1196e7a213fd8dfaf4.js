89ce247a9871c62ead990491724ad6c55ea32b19
riivo <riivo.kiljak@ger.win.int.kn>
2019-12-28 15:13:42 +0200

    Updates

---
+++ precache-manifest.1ac4c632a9714f1196e7a213fd8dfaf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764979510c7326b2f22e44a6da9cdfa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fb41d784c7aa5003b67",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "3fb41d784c7aa5003b67",
+    "url": "./static/js/2.7c80a136.chunk.js"
+  },
+  {
+    "revision": "4deb56daf92c6b6dfc0e2ef23bd38af3",
+    "url": "./static/js/2.7c80a136.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65cb455be6eaa576a5fc",
+    "url": "./static/js/main.8dca636e.chunk.js"
+  },
+  {
+    "revision": "ca97588129592a3bd2b7",
+    "url": "./static/js/runtime-main.3b529d89.js"
+  }
+]);
